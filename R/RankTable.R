071b8a5f4f37fca10c8c4f163cd9089a4b73aef9
Jerzy Wieczorek <jerzywieczorek@gmail.com>
2017-03-05 13:45:01 -0500

    Make RankTable args consistent with RankPlot; condense example dataset and its use in vignettes

--- R/RankTable.R
+++ R/RankTable.R
@@ -1,19 +1,19 @@
 
-RankTable <- function(Rank, PlaceName, Est, SE, PlaceType = "State",
+RankTable <- function(ranks, names, est, se, placeType = "State",
                      col1 = .15, col2 = .6, col3 = .85, col4 = 1,
                      textPos = 2, titleCex = 0.9, titleLift = 1.5, contentCex = 0.7,
                      pantyhoseRefLine = NULL, tikzText = NULL) {
 
-  n <- length(Rank)
+  n <- length(ranks)
 
   if(!is.null(tikzText) & isTRUE(tikzText)) {
     textR = "$\\hat{r}_k$"
-    textPlace = paste0("$", PlaceType, "\\ (k)$")
+    textPlace = paste0("$", placeType, "\\ (k)$")
     textTheta = "$\\hat{\\theta}_k$"
     textSE = "$SE_k$"
   } else {
     textR = expression(hat(r)[k])
-    textPlace = paste(PlaceType, "(k)")
+    textPlace = paste(placeType, "(k)")
     textTheta = expression(hat(theta)[k])
     textSE = expression(SE[k])
   }
@@ -29,17 +29,17 @@ RankTable <- function(Rank, PlaceName, Est, SE, PlaceType = "State",
   } else {
     plot(c(0, rep(1, n + 1)), 0:(n + 1), yaxs='i', type='n', bty='n', xaxt='n', yaxt='n', xlab='', ylab='')
   }
-  text(rep(col1, n), 1:n, Rank, cex = contentCex, pos = textPos)
+  text(rep(col1, n), 1:n, ranks, cex = contentCex, pos = textPos)
   text(col1, n + titleLift, textR, cex = titleCex, pos = textPos, font = 2)
-  text(rep(col2, n), 1:n, PlaceName, cex = contentCex, pos = textPos)
+  text(rep(col2, n), 1:n, names, cex = contentCex, pos = textPos)
   text(col2, n + titleLift, textPlace, cex = titleCex, pos = textPos, font = 2)
-  text(rep(col3, n), 1:n, Est, cex = contentCex, pos = textPos)
+  text(rep(col3, n), 1:n, est, cex = contentCex, pos = textPos)
   text(col3, n + titleLift, textTheta, cex = titleCex, pos = textPos, font = 2)
-  text(rep(col4, n), 1:n, SE, cex = contentCex, pos = textPos)
+  text(rep(col4, n), 1:n, se, cex = contentCex, pos = textPos)
   text(col4, n + titleLift, textSE, cex = titleCex, pos = textPos, font = 2)
 
   if(!is.null(pantyhoseRefLine)) {
-    text(col4, -pantyhoseRefLine, paste0("Reference ", PlaceType, ":"), cex=contentCex, pos=textPos, font=2)
+    text(col4, -pantyhoseRefLine, paste0("Reference ", placeType, ":"), cex=contentCex, pos=textPos, font=2)
   }
 }
 
