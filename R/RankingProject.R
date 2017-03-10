8c0904b58ee4c18588405552fd905fbe24f16ad3
Jerzy Wieczorek <jerzywieczorek@gmail.com>
2017-03-09 22:28:29 -0500

    Note and fix devtools::check() issues: delete files, remove tikzMetrics, fix links, add importFrom

--- R/RankingProject.R
+++ R/RankingProject.R
@@ -3,23 +3,32 @@
 #' Functions to generate plots and tables for comparing independently-sampled
 #' populations. Companion package to "A Primer on Visualizations for Comparing
 #' Populations, Including the Issue of Overlapping Confidence Intervals"
-#' (Wright, Klein, and Wieczorek, 2017, The American Statistician). See the
-#' \link[=../doc/intro.html]{Intro vignette (html)} for an overview and examples,
-#' or the \link[=../doc/primer.pdf]{Primer vignette (pdf)}
-#' for code which replicates the main figures from the article.
+#' (Wright, Klein, and Wieczorek, 2017, \emph{The American Statistician}).
+#' See the Intro vignette (html) for an overview and examples:
+#' \code{vignette("intro", package = "RankingProject")}.
+#' See the Primer vignette (pdf)
+#' for code which replicates the main figures from the article:
+#' \code{vignette("primer", package = "RankingProject")}.
 #'
 #' The "comparison" plots are based on figures and S code from
 #' "Displays for Comparing a Given State to Many Others"
-#' (Almond, Lewis, Tukey, and Yan, 2000, The American Statistician).
+#' (Almond, Lewis, Tukey, and Yan, 2000, \emph{The American Statistician}).
 #' The present package does not rely directly on Almond et al.'s S code,
 #' but draws inspiration from it. Their script was originally hosted at
 #' Statlib at \url{http://stat.cmu.edu/S/comprB} and may still be found at
 #' various Statlib mirrors, such as
 #' \url{http://ftp.uni-bayreuth.de/math/statlib/S/comprB}.
 #'
-#' The code for the "columns" plots is directly based on R's stats::heatmap()
+#' The code for the "columns" plots is directly based on R's
+#' \code{stats::heatmap()}
 #' function, with minor modifications to remove dendograms and allow the heatmap
-#' to be placed inside a larger layout().
+#' to be placed inside a larger \code{layout()}.
+#'
+#' @importFrom graphics abline arrows axis image layout legend mtext par plot
+#'   points rect segments text title
+#' @importFrom grDevices dev.flush dev.hold
+#' @importFrom stats as.dendrogram dist hclust order.dendrogram pnorm qnorm
+#'   reorder sd
 #'
 #' @docType package
 #' @name RankingProject
